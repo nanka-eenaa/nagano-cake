33246b87d5c5ecfb5e0b624f17c898289bb79f39
shostitch <102853678+shostitch@users.noreply.github.com>
2022-06-19 13:30:06 +0000

    管理者側の会員ページ作成 addressのvalidatesの追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  validates :post_code,presence: true,numericality: { only_integer: true }
+  validates :address,presence: true
+  validates :name,presence: true
 end
