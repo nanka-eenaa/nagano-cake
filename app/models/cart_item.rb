a1c1b310f7f162f7e2aafbf1df1dbb736cbcebc4
EC2 Default User <ec2-user@ip-172-31-31-192.ec2.internal>
2022-06-20 06:58:09 +0000

    cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,11 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+
+  validates :quantity, presence: true
+
+  def sub_total
+    item.tax_price * quantity
+  end
+
 end
