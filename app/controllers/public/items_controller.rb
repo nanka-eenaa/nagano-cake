816044bc4daa883f1a648c51b1ef6a48881cc621
EC2 Default User <ec2-user@ip-172-31-31-192.ec2.internal>
2022-06-20 04:06:01 +0000

    indexの表示数修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(8)
     @all_item = Item.all
     @genres = Genre.all
   end
