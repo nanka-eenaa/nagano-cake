6f1cf1fcec3cea3d457d9a2232a79d4c2b62bf21
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-06-22 09:07:50 +0000

    order_order_detail_controller finish

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,5 +10,18 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    order_details = @order.order_details.all
+    status = @order.status_before_type_cast
+    if status == 1
+      order_details.update_all(status: 1)
+    end
+    redirect_to request.referer
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:status)
+    end
 end
