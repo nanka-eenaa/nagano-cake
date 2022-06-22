7aedcb8371c9ff023550a6e00abcc688003f272f
EC2 Default User <ec2-user@ip-172-31-31-192.ec2.internal>
2022-06-22 07:35:00 +0000

    admin/order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_total_price = @order.total_payment - @order.postage
+    @order_details = @order.order_details.all
   end
 
   def update
