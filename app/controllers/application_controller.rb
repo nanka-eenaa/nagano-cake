a277f187409ed95e2e3986573bbaaa33910decd5
EC2 Default User <ec2-user@ip-172-31-31-192.ec2.internal>
2022-06-23 05:24:34 +0000

    test/rinchan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-    root_path
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_orders_path
+    else
+      root_path
+    end
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      root_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
 
   def configure_permitted_parameters
